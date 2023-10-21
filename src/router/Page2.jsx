835cc36f50b6b2ad6810f3b5b217d7497e69c1fc
Ha-ji-me <hjmx3599@gmail.com>
2023-10-21 20:12:56 +0900

    クエリパラメータ

--- src/router/Page2.jsx
+++ src/router/Page2.jsx
@@ -5,6 +5,8 @@ export const Page2 = () => {
     <div>
       <h1>Page2です</h1>
       <Link to={"/page2/999"}>URLパラメータ</Link>
+      <br />
+      <Link to={"/page2/999?name=hoge"}>Queryパラメータ</Link>
     </div>
   )
 }
